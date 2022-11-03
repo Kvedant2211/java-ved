4a12b6e4b201e9909bbeaa1d7832b263635bf5a7
Vedant <110582546+Kvedant2211@users.noreply.github.com>
2022-11-03 14:05:32 +0530

    Add files via upload

---
+++ job1.java
@@ -0,0 +1,6 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java 1"); 
+     System.out.println("Job1");
+    }  
+}  
---
+++ job3.java
@@ -0,0 +1,6 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java 3"); 
+     System.out.println("Job3");
+    }  
+}  
